2a16bd4ef0b87fd43a3c26393c6c194f6e447258
Ryan Henrick Da Silva Pinto <henrick.s1320@gmail.com>
2023-12-27 09:48:37 -0300

    Printing possible moves

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -1,14 +1,10 @@
 package boardgame;
 
 public class Position {
-	
+
 	private int row;
 	private int column;
 	
-	public Position() {
-		
-	}
-
 	public Position(int row, int column) {
 		this.row = row;
 		this.column = column;
@@ -31,16 +27,12 @@ public class Position {
 	}
 	
 	public void setValues(int row, int column) {
-		
 		this.row = row;
 		this.column = column;
-		
 	}
 	
 	@Override
-	
 	public String toString() {
 		return row + ", " + column;
 	}
-
 }
