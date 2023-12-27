1f92f20f8c47584cb3f90d46985bec2772451e6d
Ryan Henrick Da Silva Pinto <henrick.s1320@gmail.com>
2023-12-27 11:40:35 -0300

    Check logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,6 +1,5 @@
 package application;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.List;
@@ -57,8 +56,12 @@ public class UI {
 		printBoard(chessMatch.getPieces());
 		System.out.println();
 		printCapturedPieces(captured);
-		System.out.println("Turn: " + chessMatch.getTurn());
+		System.out.println();
+		System.out.println("Turn : " + chessMatch.getTurn());
 		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+		if (chessMatch.getCheck()) {
+			System.out.println("CHECK!");
+		}
 	}
 	
 	public static void printBoard(ChessPiece[][] pieces) {
@@ -102,22 +105,16 @@ public class UI {
 	}
 	
 	private static void printCapturedPieces(List<ChessPiece> captured) {
-		
 		List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE).collect(Collectors.toList());
 		List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK).collect(Collectors.toList());
-		
-		System.out.println("Captured pieces: ");
-		System.out.print("white: ");
+		System.out.println("Captured pieces:");
+		System.out.print("White: ");
 		System.out.print(ANSI_WHITE);
 		System.out.println(Arrays.toString(white.toArray()));
-		System.out.println(ANSI_RESET);
-		
-		
-		System.out.print("black: ");
+		System.out.print(ANSI_RESET);
+		System.out.print("Black: ");
 		System.out.print(ANSI_YELLOW);
 		System.out.println(Arrays.toString(black.toArray()));
-		System.out.println(ANSI_RESET);
-	
+		System.out.print(ANSI_RESET);
 	}
-	
 }
